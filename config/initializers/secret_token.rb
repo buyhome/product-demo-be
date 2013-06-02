cb59f72f1b4855d93deb3ac9cbd6f361c5e7ea67
buyhome <pai6@qq.com>
2013-06-02 15:38:14 +0800

    ini.t

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProductDemoBe::Application.config.secret_token = '034bf96530aebff88add25eb323a9be468d1ad5dfe4cfaf8c994abc146772ea3f2861668e0779418fe4102728b9cd511092a4d6f2d732c99692267a9d0cd5475'
